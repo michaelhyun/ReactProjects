e1e1392a36530e9802657a9ad2d37da618a3bb9c
Michael Hyun <michhyun@amazon.com>
2019-06-25 16:23:25 -0700

    Finish SeasonDisplay with Loading Spinner

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -1,8 +1,32 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
+import Spinner from "./Spinner";
 
-const App = () => {
-  return <div>Hello!</div>;
-};
+class App extends React.Component {
+  state = { lat: null, errorMessage: "" };
+
+  componentDidMount() {
+    navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  render() {
+    return <div className="border red">{this.renderContent()}</div>;
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    } else if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return <Spinner message="Please accept location request" />;
+  }
+}
 
 ReactDOM.render(<App />, document.querySelector("#root"));
